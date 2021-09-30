2f3f32a5cc4d401855ddea5180ab7613df00dfd0
SilvesterSpath <spath.sz@gmail.com>
2021-09-30 08:42:54 +0200

    effects

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
 
 const currentYear = new Date().getFullYear();
-
 const halloweenTime = new Date(`October 31 ${currentYear} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear;
+
+// Update countdown time
 function uptdateCountdown() {
+  countdown.classList.add('show');
   const present = new Date();
   /* present.getDate(); */
 
@@ -19,12 +25,18 @@ function uptdateCountdown() {
   const min = Math.floor(diff / 1000 / 60) % 60;
   const sec = Math.floor(diff / 1000) % 60;
 
+  // Add values to DOM
   days.innerText = d;
   hours.innerText = h < 10 ? '0' + h : h;
   minutes.innerText = min < 10 ? '0' + min : min;
   seconds.innerText = sec < 10 ? '0' + sec : sec;
 }
 
-/* uptdateCountdown(); */
+// Show spinner before countdown
+/* setTimeout(() => {
+  /* loading.remove(); */
+/* countdown.style.visibility = 'visible'; 
+}, 1000); */
 
+// Run every second
 setInterval(uptdateCountdown, 100);
